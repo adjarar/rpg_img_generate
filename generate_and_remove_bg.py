8fadf65dc72f3ab3545c7980e2926e3ec244c16a
adjarar <82608399+adjarar@users.noreply.github.com>
2023-06-01 11:13:36 +0200

    fix

--- generate_and_remove_bg.py
+++ generate_and_remove_bg.py
@@ -34,8 +34,8 @@ while len(os.listdir(prompts_dir)) > 0:
     with open(prompt_path, 'r') as prompts_file:
         prompts_file = json.load(prompts_file)
     
-    # move the prompt to the processed folder
-    shutil.move(prompt_path, processed_prompts_dir_path)
+    # delete file
+    os.remove(prompt_path)
 
     # put the json elements in a variable
     prompts_name = prompts_file["name"]
@@ -49,7 +49,6 @@ while len(os.listdir(prompts_dir)) > 0:
 
     os.makedirs(with_bg_dir_path)
     os.makedirs(without_bg_dir_path)
-    os.makedirs(processed_prompts_dir_path)
 
     # loads the correct model
     requests.post(url=f'{args.sd_url}/sdapi/v1/options', json={"sd_model_checkpoint": "fantassifiedIcons_fantassifiedIconsV20.safetensors [8340e74c3e]"})
